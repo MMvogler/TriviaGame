8e26e3c76b39ef3ee84e72b3914275ead74d67ea
Megan Vogler <mmphilmon@gmail.com>
2019-08-19 10:42:10 -0500

    added timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Global Variables
+//-----------------------------------
+var timer = "";
+// var corrAnswers: 0;
+// var incorrAnswers: 0;
+// var unansweredQs: 0;
+
+
+// Functions 
+//-----------------------------------
+setTimeout (oneMinute, 1000 * 60);
+
+function oneMinute () {
+    
+    $("#timer-done").append("<h2>All Done!</h2>");
+    console.log(oneMinute);
+}
+
+// Main Process
+//----------------------------------- 
